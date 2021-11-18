7faba195ca1d98bbba8aceb37037357b6a300e6f
maranlll <maranlll@163.com>
2021-11-18 14:37:29 +0800

    存在问题，数据类型待解决，暂忽略

--- src/main/java/entity/roadNet/roadNet/Segment.java
+++ src/main/java/entity/roadNet/roadNet/Segment.java
@@ -2,41 +2,97 @@ package entity.roadNet.roadNet;
 
 import entity.vehicle.vehicle.Vehicle;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class Segment {
+public class Segment { // 暂时不动，数据结构需修改
     private int index;
     private Lane belongLane;
     private double startPos;
     private double endPos;
-    private List<Iterator<List<Vehicle>>> vehicles;
-    private Iterator<List<Vehicle>> prev_vehicle_iter;
+    private List<Vehicle> vehicles;
+    private int curPos;
+
     public Segment() {
-
+        vehicles = new ArrayList<Vehicle>();
     }
+
     public Segment(int index, Lane belongLane, double startPos, double endPos) {
-
+        this.index = index;
+        this.belongLane = belongLane;
+        this.startPos = startPos;
+        this.endPos = endPos;
+        vehicles = new ArrayList<Vehicle>();
     }
+
     public double getStartPos() {
-        return 0;
+        return startPos;
     }
+
     public double getEndPos() {
-        return 0;
+        return endPos;
     }
+
     public int getIndex() {
-        return 0;
+        return index;
     }
-    public List<Iterator<List<Vehicle>>> getVehicles() {
-        return null;
+
+    public List<Vehicle> getVehicles() {
+        return vehicles;
     }
-    public Iterator<List<Vehicle>> findVehicle(Vehicle vehicle) {
-        return null;
+
+    public int findVehicle(Vehicle vehicle) {
+        for (int i = 0; i < vehicles.size(); i++) {
+            if (vehicles.get(i) == vehicle) {
+                return i;
+            }
+        }
+        return -1;
     }
+
     public void removeVehicle(Vehicle vehicle) {
-
+        vehicles.remove(vehicle);
     }
-    public void insertVehicle(List<Iterator<List<Vehicle>>> vehicle) {
 
+    public void insertVehicle(Vehicle vehicle) {
+        for (int i = 0; i < vehicles.size(); i++) {
+            if (vehicles.get(i).getCurDis() <= vehicle.getCurDis()) {
+                vehicles.add(i, vehicle);
+                return;
+            }
+        }
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public Lane getBelongLane() {
+        return belongLane;
+    }
+
+    public void setBelongLane(Lane belongLane) {
+        this.belongLane = belongLane;
+    }
+
+    public void setStartPos(double startPos) {
+        this.startPos = startPos;
+    }
+
+    public void setEndPos(double endPos) {
+        this.endPos = endPos;
+    }
+
+    public void setVehicles(List<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
+
+    public int getCurPos() {
+        return curPos;
+    }
+
+    public void setCurPos(int curPos) {
+        this.curPos = curPos;
     }
 }
