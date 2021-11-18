c004d649a42bda79325b4711f635512a48fa4a1c
maranlll <maranlll@163.com>
2021-11-18 14:35:55 +0800

    补充内容，完善构造函数

--- src/main/java/entity/roadNet/roadNet/Road.java
+++ src/main/java/entity/roadNet/roadNet/Road.java
@@ -1,4 +1,146 @@
 package entity.roadNet.roadNet;
 
+import entity.vehicle.vehicle.Vehicle;
+import util.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Road {
+    private String id;
+    private Intersection startIntersection;
+    private Intersection endIntersection;
+    private List<Lane> lanes;
+    private List<Point> points;
+    private List<Vehicle> planeRouteBuffer;
+
+    private void initLanePoints() { // 计算每条 lane 的 points
+        double dsum = 0;
+        List<Point> roadPoints = new ArrayList<Point>(points);
+        if (! startIntersection.isImplicitIntersection()) {
+            double width = startIntersection.getWidth();
+            Point p1 = roadPoints.get(0);
+            Point p2 = roadPoints.get(1);
+            roadPoints.set(0, p1.plus(p2.minus(p1).unit().multiply(width)));
+        }
+        if (! endIntersection.isImplicitIntersection()) {
+            double width = endIntersection.getWidth();
+            Point p1 = roadPoints.get(roadPoints.size() - 2);
+            Point p2 = roadPoints.get(roadPoints.size() - 1);
+            roadPoints.set(roadPoints.size() - 1, p1.plus(p2.minus(p1).unit().multiply(width)));
+        }
+        for (Lane lane : lanes) {
+            double dmin = dsum;
+            double dmax = dsum + lane.getWidth();
+            List<Point> lanePoints = new ArrayList<Point>();
+            for (int i = 0; i < roadPoints.size(); i++ ) {
+                Point u;
+                if (i == 0) {
+                    u = roadPoints.get(1).minus(roadPoints.get(0)).unit();
+                } else if (i + 1 == roadPoints.size()) {
+                    u = roadPoints.get(i).minus(roadPoints.get(i - 1)).unit();
+                } else {
+                    Point u1 = roadPoints.get(i + 1).minus(roadPoints.get(i)).unit();
+                    Point u2 = roadPoints.get(i).minus(roadPoints.get(i - 1)).unit();
+                    u = u1.plus(u2).unit();
+                }
+                Point v = u.normal().opposite();
+                Point point = roadPoints.get(i).plus(v.multiply((dmin + dmax) / 2));
+                lanePoints.add(point);
+            }
+            lane.setPoints(lanePoints);
+            lane.setLength(Point.getLengthOfPoints(lanePoints));
+            dsum += lane.getWidth();
+        }
+    }
+
+    public Road() {
+        lanes = new ArrayList<Lane>();
+        points = new ArrayList<Point>();
+        planeRouteBuffer = new ArrayList<Vehicle>();
+    }
+
+    public void buildSegmentationByInterval(double interval) { // TODO: segment 段数存在问题，当前为修改方案
+        for (Lane lane : lanes) {
+            lane.buildSegmentation((int) Math.max(Math.ceil(Point.getLengthOfPoints(lane.getPoints()) / interval), 1));
+        }
+    }
+
+    public boolean connectedToRoad(Road road) {
+        for (Lane lane : lanes) {
+            if (lane.getLaneLinksToRoad(road).size() != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void reset() {
+        for (Lane lane : lanes) {
+            lane.reset();
+        }
+    }
+
+    public double getWidth() {
+        double width = 0;
+        for (Lane lane : lanes) {
+            width += lane.getWidth();
+        }
+        return width;
+    }
+
+    public double getAverageLength() {
+        double sum = 0;
+        for (Lane lane : lanes) {
+            sum += lane.getLength();
+        }
+        return lanes.size() == 0 ? 0 : sum / lanes.size();
+    }
+
+    public double getAverageSpeed() {
+        int vehicleNum = 0;
+        double speedSum = 0;
+        for (Lane lane : lanes) {
+            vehicleNum += lane.getHistoryVehicleNum();
+            speedSum += lane.getHistoryAverageSpeed() * lane.getHistoryVehicleNum();
+        }
+        return vehicleNum != 0 ? speedSum / vehicleNum : -1;
+    }
+
+    public double getAverageDuration() {
+        double averageSpeed = getAverageSpeed();
+        return averageSpeed < 0 ? -1 : getAverageLength() / averageSpeed;
+    }
+
+    public void addPlanRouteVehicle(Vehicle vehicle) {
+        planeRouteBuffer.add(vehicle);
+    }
+
+    public void clearPlanRouteBuffer() {
+        planeRouteBuffer.clear();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Intersection getStartIntersection() {
+        return startIntersection;
+    }
+
+    public Intersection getEndIntersection() {
+        return endIntersection;
+    }
+
+    public List<Lane> getLanes() {
+        return lanes;
+    }
+
+    public List<Point> getPoints() {
+        return points;
+    }
+
+    public List<Vehicle> getPlaneRouteBuffer() {
+        return planeRouteBuffer;
+    }
 }
