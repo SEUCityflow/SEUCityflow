c26a064272ee86d1ec799a2dd62d93291f727fc4
maranlll <maranlll@163.com>
2021-11-18 14:36:19 +0800

    新增 get / set，完善构造函数与 reset

--- src/main/java/entity/roadNet/roadNet/RoadLink.java
+++ src/main/java/entity/roadNet/roadNet/RoadLink.java
@@ -1,5 +1,6 @@
 package entity.roadNet.roadNet;
 
+import java.util.ArrayList;
 import java.util.List;
 
 enum RoadLinkType {
@@ -12,26 +13,73 @@ public class RoadLink {
     private Intersection intersection;
     private Road startRoad;
     private Road endRoad;
-    private RoadLinkType type;;
+    private RoadLinkType type;
     private List<LaneLink> laneLinks;
     private int index;
 
-    public List<LaneLink> getLaneLinks() {
-        return null;
+    public RoadLink() {
+        laneLinks = new ArrayList<LaneLink>();
     }
-    public Road getStartRoad()  {
-        return null;
-    }
-    public Road getEndRoad()  {
-        return null;
-    }
-    public boolean isAvailable()  {
-        return false;
-    }
-    public boolean isTurn()  {
-        return false;
-    }
-    public void reset()  {
 
+    public List<LaneLink> getLaneLinks() {
+        return laneLinks;
+    }
+
+    public Road getStartRoad()  {
+        return startRoad;
+    }
+
+    public Road getEndRoad()  {
+        return endRoad;
+    }
+
+    public boolean isAvailable()  {
+        return intersection.getTrafficLight().getCurrentPhase().getRoadLinkAvailable().get(index);
+    }
+
+    public boolean isTurn()  {
+        return type == RoadLinkType.turn_left || type == RoadLinkType.turn_right;
+    }
+
+    public Intersection getIntersection() {
+        return intersection;
+    }
+
+    public void setIntersection(Intersection intersection) {
+        this.intersection = intersection;
+    }
+
+    public void setStartRoad(Road startRoad) {
+        this.startRoad = startRoad;
+    }
+
+    public void setEndRoad(Road endRoad) {
+        this.endRoad = endRoad;
+    }
+
+    public RoadLinkType getType() {
+        return type;
+    }
+
+    public void setType(RoadLinkType type) {
+        this.type = type;
+    }
+
+    public void setLaneLinks(List<LaneLink> laneLinks) {
+        this.laneLinks = laneLinks;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public void reset()  {
+        for (LaneLink laneLink : laneLinks) {
+            laneLink.reset();
+        }
     }
 }
