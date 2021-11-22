e6b64f35069ce8ced07728a597f061fadfbf11db
maranlll <maranlll@163.com>
2021-11-22 23:05:05 +0800

    修改构造函数（未彻底完成）

--- src/main/java/entity/flow/Route.java
+++ src/main/java/entity/flow/Route.java
@@ -2,10 +2,15 @@ package entity.flow;
 
 import entity.roadNet.roadNet.Road;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Route {
     private List<Road> route;
+
+    public Route() {
+        route = new ArrayList<>();
+    }
 
     public Route(List<Road> route) {
         this.route = route;
