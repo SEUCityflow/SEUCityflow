3014663ef1a16c54be79489ec85d56181608bde7
maranlll <maranlll@163.com>
2021-11-22 16:52:44 +0800

    注释掉 phase

--- src/main/java/entity/roadNet/trafficLight/LightPhase.java
+++ src/main/java/entity/roadNet/trafficLight/LightPhase.java
@@ -4,21 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LightPhase {
-    private int phase;
+//    private int phase;
     private double time;
     private List<Boolean> roadLinkAvailable;
 
     public LightPhase() {
-        roadLinkAvailable = new ArrayList<Boolean>();
+        roadLinkAvailable = new ArrayList<>();
     }
 
-    public int getPhase() {
-        return phase;
-    }
-
-    public void setPhase(int phase) {
-        this.phase = phase;
-    }
+//    public int getPhase() {
+//        return phase;
+//    }
+//
+//    public void setPhase(int phase) {
+//        this.phase = phase;
+//    }
 
     public double getTime() {
         return time;
