46e466759b737d45fdb80b5bd5136122fdf57f48
maranlll <maranlll@163.com>
2021-11-22 16:41:57 +0800

    新增 calLaneLinkPoints 与 getDirectionByDistance

--- src/main/java/util/Point.java
+++ src/main/java/util/Point.java
@@ -1,4 +1,6 @@
 package util;
+import entity.roadNet.roadNet.Lane;
+
 import java.util.*;
 
 public class Point {
@@ -55,30 +57,65 @@ public class Point {
         return ret;
     }
 
-    public double len() { // 向量长度
+    public double len() {
         return Math.sqrt(x * x + y * y);
     }
 
-    public Point normal() { // 垂直向
+    public Point normal() {
         return new Point(-y, x);
     }
 
-    public Point unit() { // 单位化
+    public Point unit() {
         double l = len();
         return new Point(x / l, y / l);
     }
 
-    public double ang() { // 向量倾角
+    public double ang() {
         return Math.atan2(y, x);
     }
 
-    public static Point getPoint(Point p1, Point p2, double a) { // p1, p2 之间占比 a 处的点坐标
+    public static List<Point> calLaneLinkPoints(Lane startLane, Lane endLane) {
+        List<Point> ret = new ArrayList<>();
+        Point start = getPointByDistance(startLane.getPoints(), startLane.getLength() - startLane.getEndIntersection().getWidth());
+        Point end = getPointByDistance(endLane.getPoints(), endLane.getLength() - endLane.getStartIntersection().getWidth());
+        double len = end.minus(start).len();
+        Point startDirection = getDirectionByDistance(startLane.getPoints(), startLane.getLength() - startLane.getEndIntersection().getWidth());
+        Point endDirection = getDirectionByDistance(endLane.getPoints(), endLane.getLength() - endLane.getStartIntersection().getWidth());
+        double minGap = 5;
+        double gap1X = startDirection.x * len * 0.5;
+        double gap1Y = startDirection.y * len * 0.5;
+        double gap2X = -endDirection.x * len * 0.5;
+        double gap2Y = -endDirection.y * len * 0.5;
+        if (gap1X * gap1X + gap1Y * gap1Y < 25 && startLane.getEndIntersection().getWidth() >= 5) {
+            gap1X = minGap * startDirection.x;
+            gap1Y = minGap * startDirection.y;
+        }
+        if (gap2X * gap2X + gap2Y * gap2Y < 25 && endLane.getStartIntersection().getWidth() >= 5) {
+            gap2X = minGap * endDirection.x;
+            gap2Y = minGap * endDirection.y;
+        }
+        Point mid1 = new Point(start.x + gap1X, start.y + gap1Y);
+        Point mid2 = new Point(end.x + gap2X, end.y + gap2Y);
+        int numPoints = 10;
+        for (int i = 0; i <= numPoints; i++) {
+            Point p1 = getPoint(start, mid1, (double)i / numPoints);
+            Point p2 = getPoint(mid1, mid2, (double)i / numPoints);
+            Point p3 = getPoint(mid2, end, (double)i / numPoints);
+            Point p4 = getPoint(p1, p2, (double)i / numPoints);
+            Point p5 = getPoint(p2, p3, (double)i / numPoints);
+            Point p6 = getPoint(p4, p5, (double)i / numPoints);
+            ret.add(new Point(p6.x, p6.y));
+        }
+        return ret;
+    }
+
+    public static Point getPoint(Point p1, Point p2, double a) {
         return new Point((p2.x - p1.x) * a + p1.x, (p2.y - p1.y) * a + p1.y);
     }
 
-    public static List<Point> calConvexHull(List<Point> points) { // 凸包
-        List<Point> ret = new ArrayList<Point>();
-        List<Point> temp = new ArrayList<Point>(points);
+    public static List<Point> calConvexHull(List<Point> points) {
+        List<Point> ret = new ArrayList<>();
+        List<Point> temp = new ArrayList<>(points);
         temp.sort(Comparator.comparing(Point::getX));
         Point p0 = temp.get(0);
         ret.add(p0);
@@ -128,15 +165,15 @@ public class Point {
         return ret;
     }
 
-    public static double crossMultiply(Point A, Point B) { // 叉乘
+    public static double crossMultiply(Point A, Point B) {
         return A.x * B.y - A.y * B.x;
     }
 
-    public static double dotMultiply(Point A, Point B) { // 点乘
+    public static double dotMultiply(Point A, Point B) {
         return A.x * B.x + A.y * B.y;
     }
 
-    public static double calcAng(Point A, Point B) { // 向量夹角
+    public static double calcAng(Point A, Point B) {
         double ang = A.ang() - B.ang();
         double pi = Math.acos(-1);
         while (ang >= pi / 2) {
@@ -148,19 +185,19 @@ public class Point {
         return Math.min(ang, pi - ang);
     }
 
-    public static Point calcIntersectionPoint(Point A, Point B, Point C, Point D) { // 向量交点
+    public static Point calcIntersectionPoint(Point A, Point B, Point C, Point D) {
         Point u = B.minus(A);
         Point v = D.minus(C);
         return A.plus(u.multiply(crossMultiply(C.minus(A), v) / crossMultiply(u, v)));
     }
 
-    public static boolean onSegment(Point A, Point B, Point C) { // C 是否在线段 AB 上
+    public static boolean onSegment(Point A, Point B, Point C) {
         double v1 = crossMultiply(B.minus(A), C.minus(A));
         double v2 = dotMultiply(C.minus(A), C.minus(A));
         return Util.sign(v1) == 0 && Util.sign(v2) <= 0;
     }
 
-    public static double getLengthOfPoints(List<Point> points) { // 线段长度
+    public static double getLengthOfPoints(List<Point> points) {
         double length = 0;
         for (int i = 0; i + 1 < points.size(); i++) {
             length += Point.minus(points.get(i + 1), points.get(i)).len();
@@ -168,7 +205,7 @@ public class Point {
         return length;
     }
 
-    public static Point getPointByDistance(List<Point> points, double dis) { // 距线段起点 dis 处的点
+    public static Point getPointByDistance(List<Point> points, double dis) {
         double distance = Math.min(Math.max(dis, 0), getLengthOfPoints(points));
         if (distance <= 0) {
             return points.get(0);
@@ -182,5 +219,18 @@ public class Point {
             }
         }
         return points.get(points.size() - 1);
+    }
+
+    public static Point getDirectionByDistance(List<Point> points, double dis) {
+        double remain = dis;
+        for (int i = 0; i + 1 < points.size(); i++) {
+            double len = points.get(i + 1).minus(points.get(i)).len();
+            if (remain < len) {
+                return points.get(i + 1).minus(points.get(i)).unit();
+            } else {
+                remain -= len;
+            }
+        }
+        return points.get(points.size() - 1).minus(points.get(points.size() - 2)).unit();
     }
 }
