a94cd605bd39699a87df631cd074c796725dd71e
maranlll <maranlll@163.com>
2021-11-23 20:12:30 +0800

    test

--- src/test/java/testLoad.java
+++ src/test/java/testLoad.java
@@ -1,5 +1,6 @@
 import entity.engine.Engine;
 import entity.flow.Flow;
+import entity.roadNet.roadNet.Intersection;
 import entity.roadNet.roadNet.RoadNet;
 
 import java.io.BufferedWriter;
@@ -17,33 +18,18 @@ public class testLoad {
 
     public static void main(String[] args) {
         try {
-//            RoadNet roadNet = new RoadNet();
-//            roadNet.loadFromJson("src/test/resources/roadnet.json");
-//            StringBuilder stringBuilder = new StringBuilder();
-//            stringBuilder.append(roadNet.toString()).append("\n");
-//            for (int i = 0; i < roadNet.getRoads().size(); i++) {
-//                stringBuilder.append(roadNet.getRoads().get(i).getId()).append("\n");
-//            }
-//            for (int i = 0; i < roadNet.getLanes().size(); i++) {
-//                stringBuilder.append(roadNet.getLanes().get(i).getId()).append("\n");
-//            }
-//            for (int i = 0; i < roadNet.getLaneLinks().size(); i++) {
-//                stringBuilder.append(roadNet.getLaneLinks().get(i).getId()).append("\n");
-//            }
-//            bufferedWriterMethod("src/test/resources/write.txt", stringBuilder);
             Engine engine = new Engine();
             try {
                 engine.loadConfig("src/test/resources/config.json");
                 StringBuilder stringBuilder = new StringBuilder();
-                stringBuilder.append(engine.getRoadNet().toString()).append("\n");
-                stringBuilder.append(engine.getFlows().size());
+                for (Intersection intersection: engine.getRoadNet().getIntersections()) {
+                    stringBuilder.append(intersection.getId()).append("\n");
+                    stringBuilder.append(intersection.getRoads().size()).append("\n");
+                }
                 bufferedWriterMethod("src/test/resources/write.txt", stringBuilder);
             } catch (Exception e) {
                 e.printStackTrace();
             }
-//
-//            System.out.println(engine.getFlows().size());
-
         } catch (Exception e) {
             e.printStackTrace();
         }
