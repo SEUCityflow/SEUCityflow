64fc5c5c38db2cb9a892ee07d3e450ef7c43ebff
Assassin718 <75885499+Assassin718@users.noreply.github.com>
2021-11-24 11:03:05 +0800

    Barrier完成

--- src/main/java/util/Barrier.java
+++ src/main/java/util/Barrier.java
@@ -6,9 +6,26 @@ public class Barrier {
     private int currCounter;
 
     public Barrier(int nb_threads) {
-
+        m_threads = nb_threads;
+        counter = new int[2];
+        currCounter = 0;
+        assert (m_threads != 0);
+        counter[0] = m_threads;
+        counter[1] = 0;
     }
-    public void Wait() {
+    public synchronized void Wait() {
+        assert (0 != counter[currCounter]);
+        try{
+            if((--counter[currCounter]) == 0){
+                currCounter = (currCounter+1)%2;
+                counter[currCounter] = m_threads;
+                this.notifyAll();
+            }else{
+                this.wait();
+                //System.out.println("aaa");
+            }
+        }catch (Exception e){
 
+        }
     }
 }
