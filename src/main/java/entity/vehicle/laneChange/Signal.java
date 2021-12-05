6c68e5c970d7f080a6e4054514c4adeb74444fc8
maranlll <maranlll@163.com>
2021-12-05 20:36:03 +0800

    新增 Signal 类

---
+++ src/main/java/entity/vehicle/laneChange/Signal.java
@@ -0,0 +1,55 @@
+package entity.vehicle.laneChange;
+
+import entity.roadNet.roadNet.Lane;
+import entity.vehicle.vehicle.Vehicle;
+
+public class Signal {
+    int urgency;
+    int direction;
+    Lane target;
+    Vehicle source;
+    //    int response;
+//    double extraSpace;
+
+    Signal() {
+    }
+
+    Signal(Signal other) {
+        urgency = other.urgency;
+        direction = other.direction;
+        target = other.target;
+        source = other.source;
+    }
+
+    public int getUrgency() {
+        return urgency;
+    }
+
+    public void setUrgency(int urgency) {
+        this.urgency = urgency;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public Lane getTarget() {
+        return target;
+    }
+
+    public void setTarget(Lane target) {
+        this.target = target;
+    }
+
+    public Vehicle getSource() {
+        return source;
+    }
+
+    public void setSource(Vehicle source) {
+        this.source = source;
+    }
+}
