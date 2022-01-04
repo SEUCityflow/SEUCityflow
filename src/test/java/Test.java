d81f3b3b124fd8700c5e37f0af82273f667550ac
maranlll <maranlll@163.com>
2022-01-04 16:41:57 +0800

    v0.4.0

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,17 +2,11 @@ import entity.engine.Engine;
 
 public class Test {
     public static void main(String[] args) {
-        Engine engine = new Engine("src/test/resources/manhattan/config.json", 5);
-//        engine.loadFromFile("");
-        System.out.println("load finished");
-        for (int i = 0; i < 750; i++) {
+        Engine engine = new Engine("src/test/resources/manhattan/config.json", 3);
+        for (int i = 0; i < 1000; i++) {
             engine.nextStep();
-            System.out.println(i + " steps finished");
-//            engine.saveToFile("");
-//            engine.loadFromFile("");
-//            engine.load(engine.snapshot());
+            System.out.println(i + " step");
         }
-//        engine.saveToFile("");
         engine.close();
     }
 }
