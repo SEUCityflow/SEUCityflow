743bfc52d67093c13ee99591d44f3c1e21062801
maranlll <maranlll@163.com>
2022-01-23 14:51:54 +0800

    修改部分 throw 异常处理

--- src/main/java/util/JsonRelate.java
+++ src/main/java/util/JsonRelate.java
@@ -40,98 +40,73 @@ public class JsonRelate {
         return strBuffer.toString();
     }
 
-    public static JSONObject getJsonMemberObject(JSONObject object, String name) throws Exception {
-        JSONObject value = null;
-        try {
-            value = object.getJSONObject(name);
-            if (value == null) {
-                throw new JsonMemberMiss(name);
-            }
-        } catch (JsonMemberMiss e) {
-            e.printStackTrace();
+    public static JSONObject getJsonMemberObject(JSONObject object, String name) throws JsonMemberMiss {
+        JSONObject value;
+        value = object.getJSONObject(name);
+        if (value == null) {
+            throw new JsonMemberMiss(name);
         }
         return value;
     }
 
-    public static JSONArray getJsonMemberArray(JSONObject object, String name) throws Exception {
-        JSONArray value = null;
-        try {
-            value = object.getJSONArray(name);
-            if (value == null) {
-                throw new JsonMemberMiss(name);
-            }
-        } catch (JsonMemberMiss e) {
-            e.printStackTrace();
+    public static JSONArray getJsonMemberArray(JSONObject object, String name) throws JsonMemberMiss {
+        JSONArray value;
+        value = object.getJSONArray(name);
+        if (value == null) {
+            throw new JsonMemberMiss(name);
         }
         return value;
     }
 
-    public static String getStringFromJsonObject(JSONObject object, String name) throws Exception {
-        String value = null;
-        try {
-            value = object.getString(name);
-            if (value == null) {
-                throw new JsonMemberMiss(name);
-            }
-        } catch (JsonMemberMiss e) {
-            e.printStackTrace();
+    public static String getStringFromJsonObject(JSONObject object, String name) throws JsonMemberMiss {
+        String value;
+        value = object.getString(name);
+        if (value == null) {
+            throw new JsonMemberMiss(name);
         }
         return value;
     }
 
-    public static int getIntFromJsonObject(JSONObject object, String name) throws Exception {
-        Integer value = null;
-        try {
-            value = object.getInteger(name);
-            if (value == null) {
-                throw new JsonMemberMiss(name);
-            }
-        } catch (JsonMemberMiss e) {
-            e.printStackTrace();
-        }
-        return value;
-
-    }
-
-    public static double getDoubleFromJsonObject(JSONObject object, String name) throws Exception {
-        Double value = null;
-        try {
-            value = object.getDouble(name);
-            if (value == null) {
-                throw new JsonMemberMiss(name);
-            }
-        } catch (JsonMemberMiss e) {
-            e.printStackTrace();
+    public static int getIntFromJsonObject(JSONObject object, String name) throws JsonMemberMiss {
+        Integer value;
+        value = object.getInteger(name);
+        if (value == null) {
+            throw new JsonMemberMiss(name);
         }
         return value;
     }
 
-    public static boolean getBooleanFromJsonObject(JSONObject object, String name) throws Exception {
-        Boolean value = null;
-        try {
-            value = object.getBoolean(name);
-            if (value == null) {
-                throw new JsonMemberMiss(name);
-            }
-        } catch (JsonMemberMiss e) {
-            e.printStackTrace();
+    public static double getDoubleFromJsonObject(JSONObject object, String name) throws JsonMemberMiss {
+        Double value;
+        value = object.getDouble(name);
+        if (value == null) {
+            throw new JsonMemberMiss(name);
+        }
+        return value;
+    }
+
+    public static boolean getBooleanFromJsonObject(JSONObject object, String name) throws JsonMemberMiss {
+        Boolean value;
+        value = object.getBoolean(name);
+        if (value == null) {
+            throw new JsonMemberMiss(name);
         }
         return Boolean.TRUE.equals(value);
     }
 
-    public static String getStringFromJsonArray(JSONArray array, int p) throws Exception {
+    public static String getStringFromJsonArray(JSONArray array, int p) {
         return array.getString(p);
     }
 
-    public static int getIntFromJsonArray(JSONArray array, int p) throws Exception {
+    public static int getIntFromJsonArray(JSONArray array, int p) {
         return array.getInteger(p);
     }
 
-    public static double getDoubleFromJsonArray(JSONArray array, int p) throws Exception {
+    public static double getDoubleFromJsonArray(JSONArray array, int p) {
         return array.getDouble(p);
     }
 
-    public static boolean getBooleanFromJsonArray(JSONArray array, int p) throws Exception {
+    public static boolean getBooleanFromJsonArray(JSONArray array, int p) {
         return array.getBoolean(p);
     }
 }
