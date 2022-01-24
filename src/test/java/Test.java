101098f5c44f5ea360a2d0962505c22e6dffed2d
maranlll <maranlll@163.com>
2022-01-24 12:24:10 +0800

    v 0.5.0

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,13 +3,13 @@ import entity.engine.Engine;
 public class Test {
     public static void main(String[] args) {
         Engine engine = new Engine("src/test/resources/test/config.json", 3);
-//        engine.loadFromFile("1000step.json");
-        for (int i = 0; i < 10; i++) {
+        engine.loadFromFile("src/test/resources/test/5000step.json");
+        for (int i = 0; i < 5000; i++) {
             engine.nextStep();
             System.out.println(i + " step");
         }
         System.out.println(engine.getFinishedVehicleCnt() + " " + engine.getAverageTravelTime());
-//        engine.saveArchiveToFile(engine.snapshot(), "1000step.json");
+//        engine.saveArchiveToFile(engine.snapshot(), "src/test/resources/test/5000step.json");
         engine.close();
     }
 }
