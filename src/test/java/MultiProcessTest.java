3a7a9e697f82614bcb100bbadb3d40677373b0d1
maranlll <872828605@qq.com>
2022-02-14 11:18:35 +0800

    test

--- src/test/java/MultiProcessTest.java
+++ src/test/java/MultiProcessTest.java
@@ -3,33 +3,24 @@ import entity.engine.Engine;
 import java.util.ArrayList;
 import java.util.List;
 
-class test implements Runnable {
+public class MultiProcessTest implements  Runnable{
+    public static void main(String[] args) {
+        List<Thread> list = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            Thread thread = new Thread(new MultiProcessTest());
+            list.add(thread);
+            thread.start();
+        }
+    }
+
     @Override
     public void run() {
         Engine engine = new Engine("src/test/resources/test/config.json", 3);
-//        for (int i = 0; i < 1000; i++) {
-//            engine.nextStep();
-//            System.out.println(this + " " + i + " step");
-//        }
-//        System.out.println(engine.getFinishedVehicleCnt() + " " + engine.getAverageTravelTime() + "!!!!!!!!!!!!!!!!!!!!!!");
+        for (int i = 0; i < 1000; i++) {
+            engine.nextStep();
+            System.out.println(this + " " + i + " step");
+        }
+        System.out.println(engine.getFinishedVehicleCnt() + " " + engine.getAverageTravelTime() + "!!!!!!!!!!!!!!!!!!!!!!");
         engine.close();
-    }
-}
-
-public class MultiProcessTest {
-    public static void main(String[] args) throws InterruptedException {
-        List<Thread> list = new ArrayList<>();
-        System.out.println("start");
-        for (int i = 0; i < 1; i++) {
-            Thread thread = new Thread(new test());
-            list.add(thread);
-            thread.start();
-
-        }
-        System.out.println("before join!!!");
-        for (Thread thread : list) {
-            thread.join();
-        }
-        System.out.println("end join");
     }
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,10 +4,10 @@ public class Test {
     public static void main(String[] args) {
         Engine engine = new Engine("src/test/resources/test/config.json", 3);
 //        engine.loadFromFile("src/test/resources/test/5000step.json");
-//        for (int i = 0; i < 5000; i++) {
-//            engine.nextStep();
-//            System.out.println(i + " step");
-//        }
+        for (int i = 0; i < 5000; i++) {
+            engine.nextStep();
+            System.out.println(i + " step");
+        }
         System.out.println(engine.getFinishedVehicleCnt() + " " + engine.getAverageTravelTime());
 //        engine.saveArchiveToFile(engine.snapshot(), "src/test/resources/test/5000step.json");
         engine.close();
