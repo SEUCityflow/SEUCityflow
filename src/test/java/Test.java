2d710f1ffa60ca5dd9532bd241edd2d88e9fbcc9
maranlll <872828605@qq.com>
2022-02-21 21:34:24 +0800

    test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,13 +3,24 @@ import entity.engine.Engine;
 public class Test {
     public static void main(String[] args) {
         Engine engine = new Engine("src/test/resources/test/config.json", 4);
-//        engine.loadFromFile("src/test/resources/test/5000step.json");
-        for (int i = 0; i < 5000; i++) {
+//        engine.loadFromFile("src/test/resources/test/2000step.json");
+//        for (int i = 0; i < 2000; i++) {
+//            engine.nextStep();
+//            System.out.println(i + " step");
+//            System.out.println(engine.getVehicleCount());
+//        }
+        int cnt = 1;
+        while (true) {
+            System.out.println(cnt + " step");
             engine.nextStep();
-            System.out.println(i + " step");
+            cnt++;
+            System.out.println(engine.getVehicleCount());
+            if (engine.getVehicleCount() == 0) {
+                break;
+            }
         }
-        System.out.println(engine.getFinishedVehicleCnt() + " " + engine.getAverageTravelTime());
-//        engine.saveArchiveToFile(engine.snapshot(), "src/test/resources/test/5000step.json");
+//        System.out.println(engine.getFinishedVehicleCnt() + " " + engine.getAverageTravelTime());
+//        engine.saveArchiveToFile(engine.snapshot(), "src/test/resources/test/2000step.json");
         engine.close();
     }
 }
