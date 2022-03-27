09874eef0f1abaeca6a7a406c4523896e33d614b
maranlll <872828605@qq.com>
2022-03-27 15:16:20 +0800

    add

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,8 +4,9 @@ public class Test {
     public static void main(String[] args) {
         Engine engine = new Engine("src/test/resources/test/config.json", 4);
 //        engine.loadFromFile("src/test/resources/test/2000step.json");
-        int N = 10000;
+        int N = 4000;
         for (int i = 0; i < N; i++) {
+            engine.nextStep();
             if (i % 50 == 0) {
                 System.out.println(i + " steps");
                 System.out.println(engine.getVehicleCount());
