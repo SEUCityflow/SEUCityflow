d30db5e27286ca5076cf0295f0acd5dcd94e144e
maranlll <872828605@qq.com>
2022-04-09 22:30:02 +0800

    update

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,11 +3,17 @@ import java.io.IOException;
 public class Test {
     public static void main(String[] args) {
         try {
-            engine eng = new engine("src/test/resources/manhattan/config.json", 4);
-//            eng.load_from_file("src/test/resources/manhattan/5000step.json");
-            int N = 5000;
+            engine eng = new engine("src/test/resources/HeFei/config.json", 4);
+//            eng.load_from_file("src/test/resources/HeFei/9000step.json");
+            int N = 10000;
             long startTime = System.currentTimeMillis(); //获取开始时间
+//            int cnt = 0;
+//            String file = "src/test/resources/HeFei/replay";
             for (int i = 0; i < N; i++) {
+//                if (cnt % 500 == 0) {
+//                    eng.set_replay_file(file + cnt + ".txt");
+//                }
+//                cnt++;
                 eng.next_step();
                 if (i % 100 == 0) {
                     System.out.println(i + " steps");
@@ -17,7 +23,7 @@ public class Test {
             long endTime = System.currentTimeMillis(); //获取结束时间
             System.out.println("程序运行时间：" + (endTime - startTime) + "ms"); //输出程序运行时间
             System.out.println(eng.get_finished_vehicle_count() + " " + eng.get_average_travel_time());
-//            eng.save_to_file("src/test/resources/manhattan/5000step.json");
+//            eng.save_to_file("src/test/resources/HeFei/9000step.json");
             eng.close();
         } catch (IOException e) {
             e.printStackTrace();
